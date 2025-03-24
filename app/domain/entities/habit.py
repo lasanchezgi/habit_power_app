7ae086daa4e4f48594ff7d089f571e93b1297fa2
Laura Sanchez Giraldo <laurasanchezgiraldo@outlook.es>
2025-03-24 14:27:28 -0500

    reestructuracion de carpetas

--- app/domain/entities/habit.py
+++ app/domain/entities/habit.py
@@ -1,12 +1,17 @@
 from pydantic import BaseModel
 from typing import List, Optional
 
+# Debe tener los mismos campos que la base de datos
+# Debo organizar esta clase para que tenga los mismos campos que la base de datos
+# Puede tener más campos para otras cosas
+# Las entidades tambien se pueden usar para hacer validaciones, tambien para generar respuestas de las APIs
 class Habit(BaseModel):
     name: str
     categories: List[str]
     date: Optional[str]
     periodicity: Optional[str]
 
+# Esta función debe estar en el habit_use_case.py
 def convert_to_habit_models(raw_habits: List[dict]) -> List[Habit]:
     habits: list[Habit] = []
 
--- app/sql/habits.sql
+++ app/sql/habits.sql
@@ -1,14 +1,16 @@
 CREATE TABLE habits (
-    id TEXT PRIMARY KEY DEFAULT (lower(hex(randomblob(4)) || '-' || hex(randomblob(2)) || '-' || '4' || substr(hex(randomblob(2)),2) || '-' || substr('89ab',abs(random()) % 4 + 1, 1) || substr(hex(randomblob(2)),2) || '-' || hex(randomblob(6)))),  -- Identificador único
+    id UUID,  -- Identificador único
     user_id TEXT,  -- Relación con el usuario
     name TEXT NOT NULL,                     -- Nombre del hábito
     description TEXT,                       -- Descripción del hábito
-    frequency TEXT CHECK (frequency IN ('daily', 'weekly', 'monthly', 'annual')), -- Frecuencia
+    frequency TEXT CHECK (frequency IN ('daily', 'weekly', 'monthly', 'annual')), -- Frecuencia, crear otra tabla para esto, esto es la periodicidad
     reminder_time TEXT,                     -- Hora de recordatorio (opcional)
-    difficulty_level TEXT CHECK (difficulty_level IN ('easy', 'medium', 'hard')), -- Nivel de dificultad
+    difficulty_level TEXT CHECK (difficulty_level IN ('easy', 'medium', 'hard')), -- Nivel de dificultad, crear otra tabla para esto
     expected_duration INTEGER,              -- Duración esperada en minutos/días
     goal_type TEXT,                         -- Tipo de meta (cantidad, tiempo, etc.)
     goal_value INTEGER,                     -- Valor de la meta (ej: 10 min de lectura)
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    -- Agreagar categorias de los habitos, tabla de las categorias, tabla que relaciona los usuarios con las categorias
+    -- Agregar la fecha que se creo el habito
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
